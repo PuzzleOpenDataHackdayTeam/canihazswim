79b5fa306be567e0bf15080020790f63b1f7f3b5
amaierhofer <a.maierhofer@gmail.com>
2012-04-27 14:05:37 +0200

    initial import of data and script

---
+++ try.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'awesome_print'
+require 'thor'
+require 'net/http'
+require 'uri'
+require 'active_support/time'
+
+
+class App < Thor
+  include Net
+  desc "get", "get document from the web"
+  def get 
+    puts HTTP.get_response(uri)
+  end 
+end
+
+
+file = 'data/T-Bulletin.html'
+doc = Nokogiri::HTML(open(file))
+header =  doc.xpath '//table//tr[position()=1]'
+column_keys = header.xpath('td').map {|td| td.text()}
+ap column_keys
+
+data = doc.xpath '//table//tr[position()>1]'
+first_row = data.first.xpath('td').map {|td| td.text()}
+
+column_keys.each_with_index do |key, index|
+  ap  "#{key}:  #{first_row[index]}"
+end
